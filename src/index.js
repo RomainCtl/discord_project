ae37574624d3e6e27c4f433e146c24fd5b987036
Alexis <alegalbtssio@gmail.com>
2019-06-19 12:17:19 +0200

    Index.js : ajout des regExp

--- src/index.js
+++ src/index.js
@@ -27,6 +27,28 @@ client.on('message', async message => {
 });
 
 
+//La liste de nos regExp qui nous serviront à récupérer les commandes : 
+
+var ban_regex = new RegExp('^!ban[ ]+<@([0-9]+)>[ ]+((?:(?!-d|-c).)+)(-d[ ]+([0-9]+))?([ ]*-c[ ]+((<#[0-9]+>[ ]*|\.audio[ ]*|\.text[ ]*)+))?[ ]*$', 'i');
+var kick_regex = new RegExp('^!kick[ ]+<@([0-9]+)>[ ]+(.*)$','i');
+var deaf_regex = new RegExp('^!deaf[ ]+<@([0-9]+)>[ ]+((?:(?!-d|-c).)+)(-d[ ]+([0-9]+))?([ ]*-c[ ]+((<#[0-9]+>[ ]*|\.audio[ ]*|\.text[ ]*)+)))?[ ]*$','i');
+var mute_regex = new RegExp('^!mute[ ]+<@([0-9]+)>[ ]+((?:(?!-d|-c).)+)(-d[ ]+([0-9]+))?([ ]*-c[ ]+((<#[0-9]+>[ ]*|\.audio[ ]*|\.text[ ]*)+)))?[ ]*$','i');
+var warn_regex = new RegExp('^!warn[ ]+@([^ ]+)[ ]+(.*)$','i');
+var create_regex = new RegExp('^!create (ban|kick|deaf|mute)[ ]+(-d[ ]+(<|>)[ ]*([0-9]+))?([ ]*-c[ ]+((NOT[ ]+)?IN)[ ]+([0-9a-z,.*]+))?[ ]*$','i');
+var cancel_regex = new RegExp('^!cancel[ ]+([0-9]+)[ ]*$','i');
+var rankup_regex = new RegExp('^!rankup[ ]+<@([0-9]+)>[ ]+([0-9]+)[ ]*$','i');
+var derank_regex = new RegExp('^!derank[ ]+<@([0-9]+)>[ ]+([0-9]+)[ ]*$','i');
+var addrole_regex = new RegExp('^!addrole[ ]+([a-z]+)[ ]*$','i');
+var delrole_regex = new RegExp('^!delrole[ ]+([0-9]+)[ ]*$','i');
+var roleAdd_regex = new RegExp('^!role[ ]+add[ ]+([0-9]+)[ ]+([0-9]+)[ ]*$','i');
+var roleDel_regex = new RegExp('^!role[ ]+del[ ]+([0-9]+)[ ]+([0-9]+)[ ]*$','i');
+var getto_regex = new RegExp('^!getto[ ]+<@([0-9]+)>[ ]*$','i');
+var getfrom_regex = new RegExp('^!getfrom[ ]+<@([0-9]+)>[ ]*$','i');
+var lock_regex = new RegExp('^!lock[ ]+((<#[0-9]+>[ ]*|\.audio[ ]*|\.text[ ]*)+)[ ]*$','i');
+var delock_regex = new RegExp('^!delock[ ]+((<#[0-9]+>[ ]*|\.audio[ ]*|\.text[ ]*)+)[ ]*$','i');
+var delmsg_regex = new RegExp('^!delmsg[ ]+((<#[0-9]+>[ ]*|\.audio[ ]*|\.text[ ]*)+)[ ]*(-d[ ]+([0-9]+))?([ ]*-u[ ]+<@([0-9]+)>)?[ ]*$','i');
+var 
+
 //On donne la clé de not bot :
 
 botToken = "NTg4MzM4NDA1MDk2Njg1NTg4.XQn3Cg.uw7DGpB4hyjKpAdVEUJxQftYGss"

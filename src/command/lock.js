d3a620e0754de2d10ba77f5e7f1917c09646b65d
Alexis <alegalbtssio@gmail.com>
2019-06-24 10:51:03 +0200

    lock.js : added

--- src/command/lock.js
+++ src/command/lock.js
@@ -4,6 +4,16 @@ module.exports = function(res, guild, channel, author, content, mentions) {
     // client param is db client
     console.log("it's a ban");
     // TODO
-
+    //On change la persmission du channel pour le verrouiller:
+    
+    channel.overwritePermissions(
+        "Fermer",//Il faudra rajouter un rôle Fermer pour indiquer que la chaine est fermer
+        { 'SEND_MESSAGES': false },
+        // optional 'reason' for permission overwrite
+        'La discussion est terminé'
+    )
+    // handle responses / errors
+    .then(console.log)
+    .catch(console.log);
     return {};
 }

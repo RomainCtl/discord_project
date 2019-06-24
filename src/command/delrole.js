e0e2d9c17b5f8f1fee676a2e9a2f30796e52a3ca
RomainCtl <romain.chantrel35@gmail.com>
2019-06-24 11:03:14 +0200

    delrole

--- src/command/delrole.js
+++ src/command/delrole.js
@@ -1,9 +1,28 @@
 const db  = require('../model');
 
-module.exports = function(res, guild, channel, author, content, mentions) {
-    // client param is db client
-    console.log("it's a ban");
-    // TODO
-
-    return {};
+module.exports = function(match, guild, channel, author, content, mentions) {
+    let fields = [{
+        name: 'Command exécuté :',
+        value: content
+    }];
+    return db.query('DELETE FROM role WHERE id = $1 RETURNING name;', [match[1]]) // res[1] = id du role
+    .then (res => {
+        if (res.rowCount == 0) {
+            fields.push({
+                name: 'Role inexistant !',
+                value: match[1]
+            });
+            return {field: fields};
+        } else {
+            fields.push({
+                name: 'Nom du role supprimer :',
+                value: res.rows[0].name,
+                inline: true
+            });
+            return {field: fields};
+        }
+    })
+    .catch( err => {
+        throw err;
+    });
 }

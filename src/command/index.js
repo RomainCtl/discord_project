6b1934867392d3c99d32d8bf96c269403ef1ce36
RomainCtl <romain.chantrel35@gmail.com>
2019-06-24 08:37:09 +0200

    add default file for commands effects

--- src/command/index.js
+++ src/command/index.js
@@ -9,91 +9,91 @@ const global_cmd = {
     kick: {
         id: 2,
         regex: new RegExp('^!kick[ ]+<@\!?([0-9]+)>[ ]+(.*)$','i'),
-        callfunc: ''
+        callfunc: require('./kick')
     },
     deaf: {
         id: 3,
         regex: new RegExp('^!deaf[ ]+<@\!?([0-9]+)>[ ]+((?:(?!-d|-c).)+)(-d[ ]+([0-9]+))?([ ]*-c[ ]+((<#[0-9]+>[ ]*|\.audio[ ]*|\.text[ ]*)+))?[ ]*$','i'),
-        callfunc: ''
+        callfunc: require('./deaf')
     },
     mute: {
         id: 4,
         regex: new RegExp('^!mute[ ]+<@\!?([0-9]+)>[ ]+((?:(?!-d|-c).)+)(-d[ ]+([0-9]+))?([ ]*-c[ ]+((<#[0-9]+>[ ]*|\.audio[ ]*|\.text[ ]*)+))?[ ]*$','i'),
-        callfunc: ''
+        callfunc: require('./mute')
     },
     warn: {
         id: 5,
         regex: new RegExp('^!warn[ ]+<@\!?([0-9]+)>[ ]+(.*)$','i'),
-        callfunc: ''
+        callfunc: require('./warn')
     },
     create: {
         id: 6,
         regex: new RegExp('^!create (ban|kick|deaf|mute)[ ]+(-d[ ]+(<|>)[ ]*([0-9]+))?([ ]*-c[ ]+((NOT[ ]+)?IN)[ ]+([0-9a-z,.*]+))?[ ]*$','i'),
-        callfunc: ''
+        callfunc: require('./create')
     },
     cancel: {
         id: 7,
         regex: new RegExp('^!cancel[ ]+([0-9]+)[ ]*$','i'),
-        callfunc: ''
+        callfunc: require('./cancel')
     },
     rankup: {
         id: 8,
         regex: new RegExp('^!rankup[ ]+<@\!?([0-9]+)>[ ]+([0-9]+)[ ]*$','i'),
-        callfunc: ''
+        callfunc: require('./rankup')
     },
     derank: {
         id: 9,
         regex: new RegExp('^!derank[ ]+<@\!?([0-9]+)>[ ]+([0-9]+)[ ]*$','i'),
-        callfunc: ''
+        callfunc: require('./derank')
     },
     addrole: {
         id: 10,
         regex: new RegExp('^!addrole[ ]+([a-z]+)[ ]*$','i'),
-        callfunc: ''
+        callfunc: require('./addrole')
     },
     delrole: {
         id: 11,
         regex: new RegExp('^!delrole[ ]+([0-9]+)[ ]*$','i'),
-        callfunc: ''
+        callfunc: require('./delrole')
     },
     roleAdd: {
         id: 12,
         regex: new RegExp('^!role[ ]+add[ ]+([0-9]+)[ ]+([0-9]+)[ ]*$','i'),
-        callfunc: ''
+        callfunc: require('./role_add')
     },
     roleDel: {
         id: 13,
         regex: new RegExp('^!role[ ]+del[ ]+([0-9]+)[ ]+([0-9]+)[ ]*$','i'),
-        callfunc: ''
+        callfunc: require('./role_del')
     },
     getto: {
         id: 14,
         regex: new RegExp('^!getto[ ]+<@\!?([0-9]+)>[ ]*$','i'),
-        callfunc: ''
+        callfunc: require('./getto')
     },
     getfrom: {
         id: 15,
         regex: new RegExp('^!getfrom[ ]+<@\!?([0-9]+)>[ ]*$','i'),
-        callfunc: ''
+        callfunc: require('./getfrom')
     },
     lock: {
         id: 16,
         regex: new RegExp('^!lock[ ]+((<#[0-9]+>[ ]*|\.audio[ ]*|\.text[ ]*)+)[ ]*$','i'),
-        callfunc: ''
+        callfunc: require('./lock')
     },
     delock: {
         id: 17,
         regex: new RegExp('^!delock[ ]+((<#[0-9]+>[ ]*|\.audio[ ]*|\.text[ ]*)+)[ ]*$','i'),
-        callfunc: ''
+        callfunc: require('./delock')
     },
     delmsg: {
         id: 18,
         regex: new RegExp('^!delmsg[ ]+((<#[0-9]+>[ ]*|\.audio[ ]*|\.text[ ]*)+)[ ]*(-d[ ]+([0-9]+))?([ ]*-u[ ]+<@\!?([0-9]+)>)?[ ]*$','i'),
-        callfunc: ''
+        callfunc: require('./delmsg')
     }
 }
 
-// TODO check les custom commands (les custom ban dans le callfunc de ban etc ...)
+// TODO check les custom commands (les custom ban dans le callfunc de ban etc ...) (PS: sinon elles seront prise en "inconnu" ou en ban normal)
 
 module.exports = {
     check_and_run: (guild, channel, author, content, mentions) => {
---
+++ src/command/kick.js
@@ -0,0 +1,9 @@
+const db  = require('../model');
+
+module.exports = function(res, guild, channel, author, content, mentions) {
+    // client param is db client
+    console.log("it's a ban");
+    // TODO
+
+    return {};
+}

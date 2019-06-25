c917351f3dce5b6ac2ee205c7e41d065bc0ba5b3
RomainCtl <romain.chantrel35@gmail.com>
2019-06-25 09:55:15 +0200

    role add

--- src/command/role_add.js
+++ src/command/role_add.js
@@ -1,9 +1,28 @@
 const db  = require('../model');
 
-module.exports = function(res, guild, channel, author, content, mentions) {
-    // client param is db client
-    console.log("it's a ban");
-    // TODO
-
-    return {};
+module.exports = function(match, guild, channel, author, content, mentions) {
+    let fields = [{
+        name: 'Command exécuté :',
+        value: content
+    }];
+    return db.query('INSERT INTO role_cmd (role_id, cmd_id) VALUES ($1, $2);', [match[1], match[2]]) // res[1] = role_id, res[2] cmd_id
+    .then (res => {
+        fields.push({
+            name: 'Message',
+            value: "Commande ajouté avec succès au rôle "+match[1]+" !",
+            inline: true
+        });
+        return {field: fields};
+    })
+    .catch( err => {
+        if (err.code == '23503' || err.code == 'P0001') {
+            fields.push({
+                name: 'Erreur :',
+                value: err.detail
+            });
+            return {field: fields};
+        } else {
+            throw err;
+        }
+    });
 }

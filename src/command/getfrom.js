1c5ac594a6f85dd56b7f988d23b5508d87149937
RomainCtl <romain.chantrel35@gmail.com>
2019-06-27 17:02:35 +0200

    fix max message length

--- src/command/getfrom.js
+++ src/command/getfrom.js
@@ -9,17 +9,19 @@ module.exports = function(match, guild, channel, author, content, mentions, bot)
     //On va chercher dans la base de données la liste des sanctions d'un modérateur sur un serveur :
     return db.query('SELECT * FROM sanction WHERE author = $1 AND serveur_id = $2;', [user.id, guild.id])
     .then( res => {
-        //On récupère les valeurs  dans un tableau qui nous permettra de les organiser avant de les afficher sur Discord :
-        let value = "```markdown\n| id | reason | duration | date | channels | victim | author | serveur_id | s_type |\n";
-        value += "|:--:|:------:|:--------:|:----:|:--------:|:------:|:------:|:----------:|:------:|\n";
-       //On récupère les infos de la base de données et on les affiches dans le tableau :
-        for (let i=0 ; i<res.rows.length ; i++) {
-            let row = res.rows[i];
-            value+= '| '+row.id+' | '+row.reason+' | '+row.duration+' | '+row.date+' | '+row.channels+' | <@'+row.victim+'> | <@'+row.author+'> | '+row.serveur_id+' | '+row.s_type+" |\n";
+        let i=0;
+        let value = 0;
+        while (i < res.rows.length) {
+            value = "```markdown\n| id | reason | duration | date | channels | victim | author | serveur_id | s_type |\n";
+            value += "|:--:|:------:|:--------:|:----:|:--------:|:------:|:------:|:----------:|:------:|\n";
+            do { // message max size is 2000...
+                let row = res.rows[i];
+                value += '| '+row.id+' | '+row.reason+' | '+row.duration+' | '+row.date+' | '+row.channels+' | <@'+row.victim+'> | <@'+row.author+'> | '+row.serveur_id+' | '+row.s_type+' |\n';
+                i++;
+            } while (i<res.rows.length && value.length < 1500);
+            // envoie le res (et donc plusieurs si il y a trop de ligne)
+            channel.send(value+"```");
         }
-
-        // envoie le res
-        channel.send(value+"```");
         // retourne pour les logs
         return {field: fields};
     })
--- src/command/getto.js
+++ src/command/getto.js
@@ -9,17 +9,19 @@ module.exports = function(match, guild, channel, author, content, mentions, bot)
     //On récupère de la base de données les sanctions liées à un utilisateur sur un serveur
     return db.query('SELECT * FROM sanction WHERE victim = $1 AND serveur_id = $2;', [user.id, guild.id])
     .then( res => {
-        //On crée un tableau pour afficher les résultats :
-        let value = "```markdown\n| id | reason | duration | date | channels | victim | author | serveur_id | s_type |\n";
-        value += "|:--:|:------:|:--------:|:----:|:--------:|:------:|:------:|:----------:|:------:|\n";
-       //On insère les résultats dans le tableau
-        for (let i=0 ; i<res.rows.length ; i++) {
-            let row = res.rows[i];
-            value+= '| '+row.id+' | '+row.reason+' | '+row.duration+' | '+row.date+' | '+row.channels+' | <@'+row.victim+'> | <@'+row.author+'> | '+row.serveur_id+' | '+row.s_type+' |\n';
+        let i=0;
+        let value = "";
+        while (i < res.rows.length) {
+            value = "```markdown\n| id | reason | duration | date | channels | victim | author | serveur_id | s_type |\n";
+            value += "|:--:|:------:|:--------:|:----:|:--------:|:------:|:------:|:----------:|:------:|\n";
+            do { // message max size is 2000...
+                let row = res.rows[i];
+                value += '| '+row.id+' | '+row.reason+' | '+row.duration+' | '+row.date+' | '+row.channels+' | <@'+row.victim+'> | <@'+row.author+'> | '+row.serveur_id+' | '+row.s_type+' |\n';
+                i++;
+            } while (i<res.rows.length && value.length < 1500);
+            // envoie le res (et donc plusieurs si il y a trop de ligne)
+            channel.send(value+"```");
         }
-
-        // envoie le res
-        channel.send(value+"```");
         // retourne pour les logs
         return {field: fields};
     })
